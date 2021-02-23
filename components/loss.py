74343305f813bebaf6aec98202c0897f3152419d
kaushal28 <shah.kaushal95@gmail.com>
2021-02-23 13:00:53 +0530

    revert

--- components/loss.py
+++ components/loss.py
@@ -10,132 +10,132 @@ from torch.nn import CTCLoss
 from torch.nn import functional as F
 
 
-# class DetectionLoss(nn.Module):
-#     """Definition of Detection Loss."""
-
-#     def __init__(self, config):
-#         super().__init__()
-#         self.config = config
-    
-#     def forward(self, y_true_clf, y_pred_clf, y_true_reg, y_pred_reg, training_mask):
-#         """
-#         Detection loss consists of classification loss and regression loss.
-
-#         y_true_clf and y_pred_clf are of shape 1 * img_size/4 * img_size/4 (pred of score map)
-#         y_true_reg and y_pred_reg are of shape 1 * img_size/4 * img_size/4 (pred of goe map)
-#         """
-
-#         # Classification loss
-#         # clf_loss = self._cross_entropy_loss(y_true_clf, y_pred_clf)
-#         clf_loss = self._dice_coefficient(y_true_clf, y_pred_clf, training_mask) * 0.01
-
-#         # Regression loss
-        
-#         # 1. IoU loss
-#         # split the regression map by channel. Each channel has shape 1 * img_size/4 * img_size/4
-#         top_gt, right_gt, bottom_gt, left_gt, theta_gt = torch.split(
-#             y_true_reg, split_size_or_sections=1, dim=1
-#         )
-#         top_pred, right_pred, bottom_pred, left_pred, theta_pred = torch.split(
-#             y_pred_reg, split_size_or_sections=1, dim=1
-#         )
-
-#         # Per pixel area calculation for corresponding bbox
-#         # sum of left + right will give width and sum of top + bottom will give height for each pixel in bbox
-#         # and multiplication of height and width is area 
-#         area_gt = (top_gt + bottom_gt) * (right_gt + left_gt)
-#         area_pred = (top_pred + bottom_pred) * (right_pred + left_pred)
-
-#         # Now calc. area of intersection height and width and then area of intersection
-#         w_int = torch.min(right_gt, right_pred) + torch.min(left_gt, left_pred)
-#         h_int = torch.min(top_gt, top_pred) + torch.min(bottom_gt, bottom_pred)
-
-#         # Area of intersection between gt and prediction
-#         area_int = w_int * h_int
-
-#         # From simple set theory
-#         area_union = area_gt + area_pred - area_int
-
-#         iou_loss = -torch.log((area_int+1) / (1+area_union))  # +1 is to prevent 0 as log(0) = -inf.
-#         angle_loss = 1 - torch.cos(theta_pred - theta_gt)
-
-#         # Regression loss. It consists of IoU loss + bbox rotation angle loss
-#         regression_loss = iou_loss + self.config["fots_hyperparameters"]["lam_theta"] * angle_loss
-
-#         # For regression loss, only consider the loss for the pixels where the ground truth
-#         # bboxes are present.
-#         regression_loss = torch.mean(regression_loss * y_true_clf * training_mask)
-
-#         # Merge the reg loss and clf loss using hyperparameter lambda reg. which
-#         # keeps balance between two losses
-#         detection_loss = clf_loss + self.config["fots_hyperparameters"]["lam_reg"] * regression_loss
-
-#         return detection_loss
-    
-#     def _dice_coefficient(self, y_true_cls, y_pred_cls,
-#                          training_mask):
-#         eps = 1e-5
-#         intersection = torch.sum(y_true_cls * y_pred_cls * training_mask)
-#         union = torch.sum(y_true_cls * training_mask) + torch.sum(y_pred_cls * training_mask) + eps
-#         loss = 1. - (2 * intersection / union)
-
-#         return loss
-    
-#     def _cross_entropy_loss(self, y_true_clf, y_pred_clf, training_mask):
-#         """
-#         Calculates cross entropy loss between per pixel prediction score map
-#         and ground truths.
-#         """
-#         return torch.nn.functional.binary_cross_entropy(y_pred_clf*training_mask, (y_true_clf*training_mask))
-
-
 class DetectionLoss(nn.Module):
-    def __init__(self):
-        super(DetectionLoss, self).__init__()
+    """Definition of Detection Loss."""
 
-    def forward(self, y_true_cls, y_pred_cls,
-                y_true_geo, y_pred_geo,
-                training_mask):
-        classification_loss = self.__dice_coefficient(y_true_cls, y_pred_cls, training_mask)
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+    
+    def forward(self, y_true_clf, y_pred_clf, y_true_reg, y_pred_reg, training_mask):
+        """
+        Detection loss consists of classification loss and regression loss.
 
-        #classification_loss = self.__cross_entroy(y_true_cls, y_pred_cls, training_mask)
-        # scale classification loss to match the iou loss part
-        classification_loss *= 0.01
+        y_true_clf and y_pred_clf are of shape 1 * img_size/4 * img_size/4 (pred of score map)
+        y_true_reg and y_pred_reg are of shape 1 * img_size/4 * img_size/4 (pred of goe map)
+        """
 
-        # d1 -> top, d2->right, d3->bottom, d4->left
-        #     d1_gt, d2_gt, d3_gt, d4_gt, theta_gt = tf.split(value=y_true_geo, num_or_size_splits=5, axis=3)
-        d1_gt, d2_gt, d3_gt, d4_gt, theta_gt = torch.split(y_true_geo, 1, 1)
-        #     d1_pred, d2_pred, d3_pred, d4_pred, theta_pred = tf.split(value=y_pred_geo, num_or_size_splits=5, axis=3)
-        d1_pred, d2_pred, d3_pred, d4_pred, theta_pred = torch.split(y_pred_geo, 1, 1)
-        area_gt = (d1_gt + d3_gt) * (d2_gt + d4_gt)
-        area_pred = (d1_pred + d3_pred) * (d2_pred + d4_pred)
-        w_union = torch.min(d2_gt, d2_pred) + torch.min(d4_gt, d4_pred)
-        h_union = torch.min(d1_gt, d1_pred) + torch.min(d3_gt, d3_pred)
-        area_intersect = w_union * h_union
-        area_union = area_gt + area_pred - area_intersect
-        L_AABB = -torch.log((area_intersect + 1.0) / (area_union + 1.0))
-        L_theta = 1 - torch.cos(theta_pred - theta_gt)
-        L_g = L_AABB + 20 * L_theta
+        # Classification loss
+        # clf_loss = self._cross_entropy_loss(y_true_clf, y_pred_clf)
+        clf_loss = self._dice_coefficient(y_true_clf, y_pred_clf, training_mask) * 0.01
 
-        # return torch.sum(L_g * y_true_cls * training_mask)/ torch.count_nonzero(y_true_cls * training_mask) + classification_loss
+        # Regression loss
+        
+        # 1. IoU loss
+        # split the regression map by channel. Each channel has shape 1 * img_size/4 * img_size/4
+        top_gt, right_gt, bottom_gt, left_gt, theta_gt = torch.split(
+            y_true_reg, split_size_or_sections=1, dim=1
+        )
+        top_pred, right_pred, bottom_pred, left_pred, theta_pred = torch.split(
+            y_pred_reg, split_size_or_sections=1, dim=1
+        )
 
-        return torch.mean(L_g * y_true_cls * training_mask) + classification_loss
+        # Per pixel area calculation for corresponding bbox
+        # sum of left + right will give width and sum of top + bottom will give height for each pixel in bbox
+        # and multiplication of height and width is area 
+        area_gt = (top_gt + bottom_gt) * (right_gt + left_gt)
+        area_pred = (top_pred + bottom_pred) * (right_pred + left_pred)
 
-    def __dice_coefficient(self, y_true_cls, y_pred_cls,
+        # Now calc. area of intersection height and width and then area of intersection
+        w_int = torch.min(right_gt, right_pred) + torch.min(left_gt, left_pred)
+        h_int = torch.min(top_gt, top_pred) + torch.min(bottom_gt, bottom_pred)
+
+        # Area of intersection between gt and prediction
+        area_int = w_int * h_int
+
+        # From simple set theory
+        area_union = area_gt + area_pred - area_int
+
+        iou_loss = -torch.log((area_int+1) / (1+area_union))  # +1 is to prevent 0 as log(0) = -inf.
+        angle_loss = 1 - torch.cos(theta_pred - theta_gt)
+
+        # Regression loss. It consists of IoU loss + bbox rotation angle loss
+        regression_loss = iou_loss + self.config["fots_hyperparameters"]["lam_theta"] * angle_loss
+
+        # For regression loss, only consider the loss for the pixels where the ground truth
+        # bboxes are present.
+        regression_loss = torch.mean(regression_loss * y_true_clf * training_mask)
+
+        # Merge the reg loss and clf loss using hyperparameter lambda reg. which
+        # keeps balance between two losses
+        detection_loss = clf_loss + self.config["fots_hyperparameters"]["lam_reg"] * regression_loss
+
+        return detection_loss
+    
+    def _dice_coefficient(self, y_true_cls, y_pred_cls,
                          training_mask):
-        '''
-        dice loss
-        :param y_true_cls:
-        :param y_pred_cls:
-        :param training_mask:
-        :return:
-        '''
         eps = 1e-5
         intersection = torch.sum(y_true_cls * y_pred_cls * training_mask)
         union = torch.sum(y_true_cls * training_mask) + torch.sum(y_pred_cls * training_mask) + eps
         loss = 1. - (2 * intersection / union)
 
         return loss
+    
+    def _cross_entropy_loss(self, y_true_clf, y_pred_clf, training_mask):
+        """
+        Calculates cross entropy loss between per pixel prediction score map
+        and ground truths.
+        """
+        return torch.nn.functional.binary_cross_entropy(y_pred_clf*training_mask, (y_true_clf*training_mask))
+
+
+# class DetectionLoss(nn.Module):
+#     def __init__(self):
+#         super(DetectionLoss, self).__init__()
+
+#     def forward(self, y_true_cls, y_pred_cls,
+#                 y_true_geo, y_pred_geo,
+#                 training_mask):
+#         classification_loss = self.__dice_coefficient(y_true_cls, y_pred_cls, training_mask)
+
+#         #classification_loss = self.__cross_entroy(y_true_cls, y_pred_cls, training_mask)
+#         # scale classification loss to match the iou loss part
+#         classification_loss *= 0.01
+
+#         # d1 -> top, d2->right, d3->bottom, d4->left
+#         #     d1_gt, d2_gt, d3_gt, d4_gt, theta_gt = tf.split(value=y_true_geo, num_or_size_splits=5, axis=3)
+#         d1_gt, d2_gt, d3_gt, d4_gt, theta_gt = torch.split(y_true_geo, 1, 1)
+#         #     d1_pred, d2_pred, d3_pred, d4_pred, theta_pred = tf.split(value=y_pred_geo, num_or_size_splits=5, axis=3)
+#         d1_pred, d2_pred, d3_pred, d4_pred, theta_pred = torch.split(y_pred_geo, 1, 1)
+#         area_gt = (d1_gt + d3_gt) * (d2_gt + d4_gt)
+#         area_pred = (d1_pred + d3_pred) * (d2_pred + d4_pred)
+#         w_union = torch.min(d2_gt, d2_pred) + torch.min(d4_gt, d4_pred)
+#         h_union = torch.min(d1_gt, d1_pred) + torch.min(d3_gt, d3_pred)
+#         area_intersect = w_union * h_union
+#         area_union = area_gt + area_pred - area_intersect
+#         L_AABB = -torch.log((area_intersect + 1.0) / (area_union + 1.0))
+#         L_theta = 1 - torch.cos(theta_pred - theta_gt)
+#         L_g = L_AABB + 20 * L_theta
+
+#         # return torch.sum(L_g * y_true_cls * training_mask)/ torch.count_nonzero(y_true_cls * training_mask) + classification_loss
+
+#         return torch.mean(L_g * y_true_cls * training_mask) + classification_loss
+
+#     def __dice_coefficient(self, y_true_cls, y_pred_cls,
+#                          training_mask):
+#         '''
+#         dice loss
+#         :param y_true_cls:
+#         :param y_pred_cls:
+#         :param training_mask:
+#         :return:
+#         '''
+#         eps = 1e-5
+#         intersection = torch.sum(y_true_cls * y_pred_cls * training_mask)
+#         union = torch.sum(y_true_cls * training_mask) + torch.sum(y_pred_cls * training_mask) + eps
+#         loss = 1. - (2 * intersection / union)
+
+#         return loss
 
 
 class RecognitionLoss(nn.Module):
@@ -171,7 +171,7 @@ class FOTSLoss(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.rec_loss = RecognitionLoss()
-        self.det_loss = DetectionLoss()
+        self.det_loss = DetectionLoss(config)
         self.config = config
     
     def forward(
--- components/shared_convolutions.py
+++ components/shared_convolutions.py
@@ -4,211 +4,81 @@ import torch
 import math
 
 
-# class SharedConvolutions(nn.Module):
-#     """
-#     Shared convolution layers to extract shared features for both
-#     text detection and recognition branch of FOTS.
-#     """
-
-#     def __init__(self, back_bone: nn.Module):
-#         """Constructor."""
-#         super().__init__()
-#         self.back_bone = back_bone
-
-#         # As we are using backbone as feature extractor,
-#         # set it to evaluation mode.
-#         self.back_bone.eval()
-
-#         # Freeze the weights of backbone (because it's feature extractor).
-#         for param in self.back_bone.parameters():
-#             param.required_grad = False
-        
-#         # Define concat layers
-#         self.concat4 = ConcatLayer(2048 + 1024, 128)
-#         self.concat3 = ConcatLayer(128 + 512, 64)
-#         self.concat2 = ConcatLayer(64 + 256, 32)
-
-#         self.conv = nn.Conv2d(32, 32, kernel_size = 3, padding = 1)
-#         self.bn = nn.BatchNorm2d(32, momentum=0.003)
-    
-#     def forward(self, x):
-#         """Module's forward pass."""
-
-#         # Subtract the mean from the image.
-#         x = self._mean_image_subtraction(x)
-
-#         # First extract features using back bone network.
-#         res5, res4, res3, res2 = self._extract_features(x)
-
-#         # As per the paper, res5 is not concatenated with any
-#         # high level feature map. So apply deconv directly
-#         res5_deconv = self._deconv(res5)
-
-#         # Concat deconv'ed res5 with res4
-#         res4_concat = self.concat4(res5_deconv, res4)
-#         res4_deconv = self._deconv(res4_concat)
-
-#         # Concat deconv'ed res4 with res3
-#         res3_concat = self.concat3(res4_deconv, res3)
-#         res3_deconv = self._deconv(res3_concat)
-
-#         # Concat deconv'ed res3 with res2
-#         res2_concat = self.concat2(res3_deconv, res2)
-        
-#         # Pass the final output to 1 conv and bn layers
-#         output = self.conv(res2_concat)
-#         output = self.bn(output)
-#         output = F.relu(output)
-
-#         return output
-
-#     def _extract_features(self, x):
-#         """Extract features from given input and backbone."""
-#         x = self.back_bone.conv1(x)
-#         x = self.back_bone.bn1(x)
-#         x = self.back_bone.relu(x)
-#         x = self.back_bone.maxpool(x)
-#         res2 = self.back_bone.layer1(x)
-#         res3 = self.back_bone.layer2(res2)
-#         res4 = self.back_bone.layer3(res3)
-#         res5 = self.back_bone.layer4(res4)
-
-#         return res5, res4, res3, res2
-    
-#     def _mean_image_subtraction(self, images, means=[123.68, 116.78, 103.94]):
-#         """
-#         Image Standardization. Subtracts the mean from the given image.
-#         """
-#         num_channels = images.data.shape[1]
-#         if len(means) != num_channels:
-#             raise ValueError('len(means) must match the number of channels')
-#         for i in range(num_channels):
-#             images.data[:, i, :, :] -= means[i]
-
-#         return images
-
-#     def _deconv(self, feature):
-#         """
-#         Apply deconv operation (inverse of pooling) on given feature map.
-#         """
-#         # Upsample the given feature.
-#         # Doc: https://pytorch.org/docs/stable/nn.functional.html#interpolate
-#         return F.interpolate(
-#             feature,
-#             mode='bilinear',
-#             scale_factor=2,  # As per the paper
-#             align_corners = True
-#         )
-    
-
-# class ConcatLayer(nn.Module):
-#     """Concatenates given feature maps."""
-    
-#     def __init__(self, in_channels, out_channels):
-#         super().__init__()
-#         self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1)
-#         self.bn1 = nn.BatchNorm2d(out_channels, momentum=0.003)
-
-#         self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
-#         self.bn2 = nn.BatchNorm2d(out_channels, momentum=0.003)
-
-#     def forward(self, prev_features, curr_features):
-#         """Forward pass."""
-#         concated_features = torch.cat([prev_features, curr_features], dim=1)
-
-#         output = self.conv1(concated_features)
-#         output = self.bn1(output)
-#         output = F.relu(output)
-
-#         output = self.conv2(output)
-#         output = self.bn2(output)
-#         output = F.relu(output)
-
-#         return output
-
 class SharedConvolutions(nn.Module):
     """
     Shared convolution layers to extract shared features for both
     text detection and recognition branch of FOTS.
     """
 
-    def __init__(self, back_bone):
+    def __init__(self, back_bone: nn.Module):
         """Constructor."""
         super().__init__()
-        self.backbone = back_bone
-        self.backbone.eval()
+        self.back_bone = back_bone
 
-        self.mergeLayers0 = DummyLayer()
+        # As we are using backbone as feature extractor,
+        # set it to evaluation mode.
+        self.back_bone.eval()
 
-        self.mergeLayers1 = ConcatLayer(2048 + 1024, 128)
-        self.mergeLayers2 = ConcatLayer(128 + 512, 64)
-        self.mergeLayers3 = ConcatLayer(64 + 256, 32)
+        # Freeze the weights of backbone (because it's feature extractor).
+        for param in self.back_bone.parameters():
+            param.required_grad = False
+        
+        # Define concat layers
+        self.concat4 = ConcatLayer(2048 + 1024, 128)
+        self.concat3 = ConcatLayer(128 + 512, 64)
+        self.concat2 = ConcatLayer(64 + 256, 32)
 
-        self.mergeLayers4 = nn.Conv2d(32, 32, kernel_size = 3, padding = 1)
-        self.bn5 = nn.BatchNorm2d(32, momentum=0.003)
+        self.conv = nn.Conv2d(32, 32, kernel_size = 3, padding = 1)
+        self.bn = nn.BatchNorm2d(32, momentum=0.003)
+    
+    def forward(self, x):
+        """Module's forward pass."""
 
-    def forward(self, input):
+        # Subtract the mean from the image.
+        x = self._mean_image_subtraction(x)
 
-        input = self._mean_image_subtraction(input)
+        # First extract features using back bone network.
+        res5, res4, res3, res2 = self._extract_features(x)
 
-        f = self._extract_features(input)
+        # As per the paper, res5 is not concatenated with any
+        # high level feature map. So apply deconv directly
+        res5_deconv = self._deconv(res5)
 
-        g = [None] * 4
-        h = [None] * 4
+        # Concat deconv'ed res5 with res4
+        res4_concat = self.concat4(res5_deconv, res4)
+        res4_deconv = self._deconv(res4_concat)
 
-        h[0] = self.mergeLayers0(f[0])
-        g[0] = self._deconv(h[0])
+        # Concat deconv'ed res4 with res3
+        res3_concat = self.concat3(res4_deconv, res3)
+        res3_deconv = self._deconv(res3_concat)
 
-        h[1] = self.mergeLayers1(g[0], f[1])
-        g[1] = self._deconv(h[1])
+        # Concat deconv'ed res3 with res2
+        res2_concat = self.concat2(res3_deconv, res2)
+        
+        # Pass the final output to 1 conv and bn layers
+        output = self.conv(res2_concat)
+        output = self.bn(output)
+        output = F.relu(output)
 
-        h[2] = self.mergeLayers2(g[1], f[2])
-        g[2] = self._deconv(h[2])
+        return output
 
-        h[3] = self.mergeLayers3(g[2], f[3])
+    def _extract_features(self, x):
+        """Extract features from given input and backbone."""
+        x = self.back_bone.conv1(x)
+        x = self.back_bone.bn1(x)
+        x = self.back_bone.relu(x)
+        x = self.back_bone.maxpool(x)
+        res2 = self.back_bone.layer1(x)
+        res3 = self.back_bone.layer2(res2)
+        res4 = self.back_bone.layer3(res3)
+        res5 = self.back_bone.layer4(res4)
 
-        final = self.mergeLayers4(h[3])
-        final = self.bn5(final)
-        final = F.relu(final)
-
-        return final
-
-    def _extract_features(self, input):
-        conv2 = None
-        conv3 = None
-        conv4 = None
-        output = None # n * 7 * 7 * 2048
-
-        for name, layer in self.backbone.named_children():
-            input = layer(input)
-            if name == 'layer1':
-                conv2 = input
-            elif name == 'layer2':
-                conv3 = input
-            elif name == 'layer3':
-                conv4 = input
-            elif name == 'layer4':
-                output = input
-                break
-
-        return output, conv4, conv3, conv2
-
-    def _deconv(self, input):
+        return res5, res4, res3, res2
+    
+    def _mean_image_subtraction(self, images, means=[123.68, 116.78, 103.94]):
         """
-        Apply deconv operation (inverse of pooling) on given feature map.
+        Image Standardization. Subtracts the mean from the given image.
         """
-        _, _, H, W = input.shape
-        # Upsample the given feature.
-        # Doc: https://pytorch.org/docs/stable/nn.functional.html#interpolate
-        return F.interpolate(input, mode = 'bilinear', scale_factor=2, align_corners=True)
-
-    def _mean_image_subtraction(self, images, means = [123.68, 116.78, 103.94]):
-        '''
-        image normalization
-        :param images: bs * w * h * channel
-        :param means:
-        :return:
-        '''
         num_channels = images.data.shape[1]
         if len(means) != num_channels:
             raise ValueError('len(means) must match the number of channels')
@@ -217,36 +87,166 @@ class SharedConvolutions(nn.Module):
 
         return images
 
-
-class DummyLayer(nn.Module):
-
-    def forward(self, input_f):
-        return input_f
-
+    def _deconv(self, feature):
+        """
+        Apply deconv operation (inverse of pooling) on given feature map.
+        """
+        # Upsample the given feature.
+        # Doc: https://pytorch.org/docs/stable/nn.functional.html#interpolate
+        return F.interpolate(
+            feature,
+            mode='bilinear',
+            scale_factor=2,  # As per the paper
+            align_corners = True
+        )
+    
 
 class ConcatLayer(nn.Module):
+    """Concatenates given feature maps."""
+    
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+        self.bn1 = nn.BatchNorm2d(out_channels, momentum=0.003)
 
-    def __init__(self, inputChannels, outputChannels):
-        """
-        :param inputChannels: channels of g+f
-        :param outputChannels:
-        """
-        super(ConcatLayer, self).__init__()
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_channels, momentum=0.003)
 
-        self.conv2dOne = nn.Conv2d(inputChannels, outputChannels, kernel_size = 1)
-        self.bnOne = nn.BatchNorm2d(outputChannels, momentum=0.003)
+    def forward(self, prev_features, curr_features):
+        """Forward pass."""
+        concated_features = torch.cat([prev_features, curr_features], dim=1)
 
-        self.conv2dTwo = nn.Conv2d(outputChannels, outputChannels, kernel_size = 3, padding = 1)
-        self.bnTwo = nn.BatchNorm2d(outputChannels, momentum=0.003)
-
-    def forward(self, inputPrevG, inputF):
-        input = torch.cat([inputPrevG, inputF], dim = 1)
-        output = self.conv2dOne(input)
-        output = self.bnOne(output)
+        output = self.conv1(concated_features)
+        output = self.bn1(output)
         output = F.relu(output)
 
-        output = self.conv2dTwo(output)
-        output = self.bnTwo(output)
+        output = self.conv2(output)
+        output = self.bn2(output)
         output = F.relu(output)
 
         return output
+
+# class SharedConvolutions(nn.Module):
+#     """
+#     Shared convolution layers to extract shared features for both
+#     text detection and recognition branch of FOTS.
+#     """
+
+#     def __init__(self, back_bone):
+#         """Constructor."""
+#         super().__init__()
+#         self.backbone = back_bone
+#         self.backbone.eval()
+
+#         self.mergeLayers0 = DummyLayer()
+
+#         self.mergeLayers1 = ConcatLayer(2048 + 1024, 128)
+#         self.mergeLayers2 = ConcatLayer(128 + 512, 64)
+#         self.mergeLayers3 = ConcatLayer(64 + 256, 32)
+
+#         self.mergeLayers4 = nn.Conv2d(32, 32, kernel_size = 3, padding = 1)
+#         self.bn5 = nn.BatchNorm2d(32, momentum=0.003)
+
+#     def forward(self, input):
+
+#         input = self._mean_image_subtraction(input)
+
+#         f = self._extract_features(input)
+
+#         g = [None] * 4
+#         h = [None] * 4
+
+#         h[0] = self.mergeLayers0(f[0])
+#         g[0] = self._deconv(h[0])
+
+#         h[1] = self.mergeLayers1(g[0], f[1])
+#         g[1] = self._deconv(h[1])
+
+#         h[2] = self.mergeLayers2(g[1], f[2])
+#         g[2] = self._deconv(h[2])
+
+#         h[3] = self.mergeLayers3(g[2], f[3])
+
+#         final = self.mergeLayers4(h[3])
+#         final = self.bn5(final)
+#         final = F.relu(final)
+
+#         return final
+
+#     def _extract_features(self, input):
+#         conv2 = None
+#         conv3 = None
+#         conv4 = None
+#         output = None # n * 7 * 7 * 2048
+
+#         for name, layer in self.backbone.named_children():
+#             input = layer(input)
+#             if name == 'layer1':
+#                 conv2 = input
+#             elif name == 'layer2':
+#                 conv3 = input
+#             elif name == 'layer3':
+#                 conv4 = input
+#             elif name == 'layer4':
+#                 output = input
+#                 break
+
+#         return output, conv4, conv3, conv2
+
+#     def _deconv(self, input):
+#         """
+#         Apply deconv operation (inverse of pooling) on given feature map.
+#         """
+#         _, _, H, W = input.shape
+#         # Upsample the given feature.
+#         # Doc: https://pytorch.org/docs/stable/nn.functional.html#interpolate
+#         return F.interpolate(input, mode = 'bilinear', scale_factor=2, align_corners=True)
+
+#     def _mean_image_subtraction(self, images, means = [123.68, 116.78, 103.94]):
+#         '''
+#         image normalization
+#         :param images: bs * w * h * channel
+#         :param means:
+#         :return:
+#         '''
+#         num_channels = images.data.shape[1]
+#         if len(means) != num_channels:
+#             raise ValueError('len(means) must match the number of channels')
+#         for i in range(num_channels):
+#             images.data[:, i, :, :] -= means[i]
+
+#         return images
+
+
+# class DummyLayer(nn.Module):
+
+#     def forward(self, input_f):
+#         return input_f
+
+
+# class ConcatLayer(nn.Module):
+
+#     def __init__(self, inputChannels, outputChannels):
+#         """
+#         :param inputChannels: channels of g+f
+#         :param outputChannels:
+#         """
+#         super(ConcatLayer, self).__init__()
+
+#         self.conv2dOne = nn.Conv2d(inputChannels, outputChannels, kernel_size = 1)
+#         self.bnOne = nn.BatchNorm2d(outputChannels, momentum=0.003)
+
+#         self.conv2dTwo = nn.Conv2d(outputChannels, outputChannels, kernel_size = 3, padding = 1)
+#         self.bnTwo = nn.BatchNorm2d(outputChannels, momentum=0.003)
+
+#     def forward(self, inputPrevG, inputF):
+#         input = torch.cat([inputPrevG, inputF], dim = 1)
+#         output = self.conv2dOne(input)
+#         output = self.bnOne(output)
+#         output = F.relu(output)
+
+#         output = self.conv2dTwo(output)
+#         output = self.bnTwo(output)
+#         output = F.relu(output)
+
+#         return output
