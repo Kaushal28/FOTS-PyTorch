94744ba675794db3165491f9b911e92bf32bc589
kaushal28 <shah.kaushal95@gmail.com>
2021-02-23 22:24:37 +0530

    modified data preprocessor

--- data_helpers/data_preprocessor.py
+++ data_helpers/data_preprocessor.py
@@ -6,7 +6,7 @@ import pandas as pd
 from torch.utils.data import DataLoader
 
 from .datasets import Synth800kDataset
-from .data_utils import synth800k_collate
+from .data_utils import icdar_collate
 
 from tqdm import tqdm
 
@@ -23,7 +23,7 @@ def preprocess(config):
         pin_memory=True,
         batch_size=config["batch_size"],
         shuffle=False,
-        collate_fn=synth800k_collate
+        collate_fn=icdar_collate
     )
 
     os.makedirs(os.path.join(config["output_dir"], "image"), exist_ok=True)
@@ -31,10 +31,10 @@ def preprocess(config):
     os.makedirs(os.path.join(config["output_dir"], "geo"), exist_ok=True)
     os.makedirs(os.path.join(config["output_dir"], "training_mask"), exist_ok=True)
 
-    img_list, sm_list, gm_list, tm_list = [], [], [], []
+    img_list, sm_list, gm_list, tm_list, bboxes, transcripts, mappings = [], [], [], [], [], [], []
     for idx, batch in tqdm(enumerate(data_loader), total=len(data_loader), position=0, leave=True):
-        image_paths, images, score_maps, geo_maps, training_masks = batch
-        for pth, i, s, g, tm in zip(image_paths, images, score_maps, geo_maps, training_masks):
+        image_paths, images, bboxs, training_masks, texts, score_maps, geo_maps, mapping = batch
+        for pth, img, bbox, txt, map, scr, geo, tm in zip(image_paths, images, bboxs, texts, mapping, score_maps, geo_maps, training_masks):
             img_pth = pth.split("/")[-2:]
             img_name = img_pth[-1].split(".")[0]
 
@@ -42,10 +42,13 @@ def preprocess(config):
             sm_list.append(f"score/{img_name}_score_map.npy")
             gm_list.append(f"geo/{img_name}_geo_map.npy")
             tm_list.append(f"training_mask/{img_name}_tm.npy")
+            bboxes.append(bbox)
+            transcripts.append(txt)
+            mappings.append(map)
 
-            np.save(f"{config['output_dir']}/image/{img_name}.npy", i.numpy().astype(np.uint8))
-            np.save(f"{config['output_dir']}/score/{img_name}_score_map.npy", s.numpy().astype(np.uint8))
-            np.save(f"{config['output_dir']}/geo/{img_name}_geo_map.npy", g.numpy().astype(np.float32))
+            np.save(f"{config['output_dir']}/image/{img_name}.npy", img.numpy().astype(np.uint8))
+            np.save(f"{config['output_dir']}/score/{img_name}_score_map.npy", scr.numpy().astype(np.uint8))
+            np.save(f"{config['output_dir']}/geo/{img_name}_geo_map.npy", geo.numpy().astype(np.float32))
             np.save(f"{config['output_dir']}/training_mask/{img_name}_tm.npy", tm.numpy().astype(np.uint8))
         
         if idx == config["num_iterations"]:
@@ -55,7 +58,10 @@ def preprocess(config):
         "images": img_list,
         "score_maps": sm_list,
         "geo_maps": gm_list,
-        "training_masks": tm_list
+        "training_masks": tm_list,
+        "bboxes": bboxes,
+        "transcripts": transcripts,
+        "mappings": mappings
     })
     data_df.to_csv(f"{config['output_dir']}/train.csv", index=False)
 
